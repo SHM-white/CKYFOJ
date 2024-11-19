e417da69828d671653d359b57b2a2da78465eab5
SHM_white <1131287310a@gmail.com>
2024-11-19 17:43:11 +0800

    333

--- exam2/a.c
+++ exam2/a.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
-int main(){
-    unsigned int memory1 = 0xFFFF;
-    
+#include <stdlib.h>
+int main()
+{
+    unsigned short CRC = 0xFFFF;
+    int n;
+    scanf("%d", &n);
+    for (size_t i = 0; i < n; i++)
+    {
+        unsigned short data;
+        scanf("%hx", &data);
+        CRC = (CRC) ^ data;
+        for (int j = 0; j < 8; j++)
+        {
+            if (CRC & 0b01)
+            {
+                CRC >>= 1;
+                CRC ^= 0xA001;
+            }
+            else
+            {
+                CRC >>= 1;
+            }
+        }
+    }
+    printf("%hx %hx", (CRC & 0xFF00) >> 8, (CRC & 0x00FF));
 }
